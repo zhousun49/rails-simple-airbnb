de9e2e7846c052b349bebc5ae9327db71b3ffb83
Zhou Sun <ZHOUSUN@USC.EDU>
2019-07-24 16:22:16 +0800

    updated bootstap

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,3 @@
+class FlatsController < ApplicationController
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all if Rails.env.development?
+
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+5.times do
+  Flat.create({ name: Faker::Company.bs, address: Faker::Address.street_address, price_per_night: rand(50..100), number_of_guests: rand(4..10), description: Faker::Quotes::Shakespeare.as_you_like_it_quote })
+end
+
+p 'created'
